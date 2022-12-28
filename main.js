d3c5d5b8a18ec1850824aba789b10a066e96efac
kienth <45379893+kienth@users.noreply.github.com>
2022-12-28 20:34:50 +0800

    module-1 project with unit-testing

--- main.js
+++ main.js
@@ -5,32 +5,64 @@
 //4. Happy coding :)
 
 //newsList is the variable that holds an array of strings
+
+// export var newsList = [
+//     "1",
+//     "2",
+//     "3",
+//     "11",
+//     "22",
+//     "33",
+//     "111",
+//     "222",
+//     "333",
+//     "1111",
+//     "2222",
+//     "3333",
+//     "11111",
+//     "22222",
+//     "33333",
+//     "111111",
+//     "222222",
+//     "333333",
+// ];
+
+//have kind of issue where after i search then click sorting bottons, some news display not moving. But when i used my example list, it works.
+
 export var newsList = [
-  "What films should an aspiring journalist watch?",
-  "Buried underpants and tea bags help scientists evaluate soil",
-  "Decoder: Mining asteroids for minerals can help spare Earth",
-  "Media glare can enrich tennis pros yet imperil mental health",
-  "'Nightmare' TV show 'Euphoria — health threat or high art?",
-  "Decoder: Armenia in a bind as Ukraine war resets global order",
-  "What books should an aspiring journalist read?",
-  "Marie Colvin shined a light on war-torn corners of the world",
+    "What films should an aspiring journalist watch?",
+    "Buried underpants and tea bags help scientists evaluate soil",
+    "Decoder: Mining asteroids for minerals can help spare Earth",
+    "Media glare can enrich tennis pros yet imperil mental health",
+    "'Nightmare' TV show 'Euphoria — health threat or high art?",
+    "Decoder: Armenia in a bind as Ukraine war resets global order",
+    "What books should an aspiring journalist read?",
+    "Marie Colvin shined a light on war-torn corners of the world",
 ];
 
 export function search() {
-  //inputValue is the variable that contains the search string
-  const inputValue = document.getElementById("search-input").value;
+    //inputValue is the variable that contains the search string
 
-  //Write your code here for the search function
+    // const inputValue = "art";  // unit testing
 
-  return newsList;
+    const inputValue = document.getElementById("search-input").value;
+
+    let searchInput = newsList.filter((newsList) =>
+        newsList.includes(inputValue)
+    );
+
+    return searchInput;
 }
 
 export function sort(type) {
-  if (type == "ascending") {
-    //Write your code here for sorting (ascending)
-  } else {
-    //Write your code here for sorting (descending)
-  }
+    let searchInput = search();
 
-  return newsList;
+    if (type == "ascending") {
+        searchInput.sort();
+        //Write your code here for sorting (ascending)
+    } else {
+        searchInput.reverse();
+        //Write your code here for sorting (descending)
+    }
+    return searchInput;
 }
--- main.test.js
+++ main.test.js
@@ -2,5 +2,17 @@ import { sort, search, newsList } from "./main.js";
 
 //Example testing for search function
 test("Search function testing", () => {
-  expect(search(newsList)).toBe("I am a cool web developer");
+    const inputValue = [
+        "Decoder: Mining asteroids for minerals can help spare Earth",
+        "'Nightmare' TV show 'Euphoria — health threat or high art?",
+    ];
+    expect(search()).toEqual(expect.arrayContaining(inputValue));
+});
+
+test("Sort function testing", () => {
+    const sortValue = [
+        "'Nightmare' TV show 'Euphoria — health threat or high art?",
+        "Decoder: Mining asteroids for minerals can help spare Earth",
+    ];
+    expect(sort("ascending")).toEqual(expect.arrayContaining(sortValue));
 });
